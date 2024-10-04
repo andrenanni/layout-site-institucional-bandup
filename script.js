3a88e96bde04e35d50d1d4d840cbbbafd76ba3ce
andrenanni <andre-nanni@hotmail.com>
2024-10-04 02:57:32 -0300

    continuação do menu

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 	// Script slide
-	/* let next = document.querySelector('.next');
+	let next = document.querySelector('.next');
 	let prev = document.querySelector('.prev');
 
 	next.addEventListener('click', function () {
@@ -11,11 +11,22 @@ document.addEventListener("DOMContentLoaded", function () {
 	prev.addEventListener('click', function () {
 		let items = document.querySelectorAll('.item')
 		document.querySelector('.slide').prepend(items[items.length - 1]);
-	}) */
-
+	})
+})
+document.addEventListener("DOMContentLoaded", function () {
 	// Script menu full
 	document.querySelector('.menu-bar').addEventListener('click', function (e) {
 		e.preventDefault();
 		document.querySelector('#menuaberto').classList.toggle('close');
 	})
+})
+
+document.querySelectorAll('a, img').forEach(drag => {
+	drag.addEventListener('dragstart', function(e){
+		e.preventDefault();
+	})
+});
+
+document.querySelector('.menu-lang').addEventListener('mouseover', function(){
+	alert("Hello World!");
 })
