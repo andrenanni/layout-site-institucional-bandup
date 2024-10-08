7242f3b3aecd62865f29848ed773a1079dd9709e
andrenanni <andre-nanni@hotmail.com>
2024-10-08 00:31:50 -0300

    montagem do novo menu scroll

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
+// Script slide
 document.addEventListener("DOMContentLoaded", function () {
-	// Script slide
 	let next = document.querySelector('.next');
 	let prev = document.querySelector('.prev');
 
 	next.addEventListener('click', function () {
 		let items = document.querySelectorAll('.item')
 		document.querySelector('.slide').appendChild(items[0]);
-	})
+	});
 
 	prev.addEventListener('click', function () {
 		let items = document.querySelectorAll('.item')
 		document.querySelector('.slide').prepend(items[items.length - 1]);
-	})
-})
+	});
+});
+
+// Script menu full
 document.addEventListener("DOMContentLoaded", function () {
-	// Script menu full
 	document.querySelector('.menu-bar').addEventListener('click', function (e) {
 		e.preventDefault();
 		document.querySelector('#menuaberto').classList.toggle('close');
-	})
+	});
 })
 
+document.querySelector('header').addEventListener('scroll', function(e){
+	e.preventDefault();
+	document.querySelector('.header-slide-menu').classList.toggle('show');
+})
+
+// remove dragstart
 document.querySelectorAll('a, img').forEach(drag => {
 	drag.addEventListener('dragstart', function(e){
 		e.preventDefault();
-	})
+	});
 });
 
-document.querySelector('.menu-lang').addEventListener('mouseover', function(){
-	alert("Hello World!");
-})
+// Script menu lang
+document.querySelector('.lang-txt').addEventListener('mouseover', function(e){
+	e.preventDefault();
+	document.querySelector('#menuaberto-lang').classList.toggle('close')
+});
