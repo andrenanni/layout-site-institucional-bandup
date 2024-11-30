c6e81f01dfae74340ca304c852c56ce3395c12a4
andrenanni <andre-nanni@hotmail.com>
2024-11-30 17:44:47 -0300

    Pasta TCC Kauã adicionada

---
+++ bandup-tcc-kaua/lojas/lojas.js
@@ -0,0 +1,41 @@
+const textos = ["Warner Bros?", "DC Comics?", "Cartoon Network?", "HBO?", "Harry Potter?", "Mônica?", "Fresno?"];
+const mudaTextoElement = document.querySelector('.muda-texto');
+let index = 0;
+let charIndex = 0;
+let isDeleting = false;
+let typingSpeed = 100; 
+let pauseBetweenTexts = 1500; 
+
+function typeEffect() {
+    const currentText = textos[index];
+
+    if (isDeleting) {
+        mudaTextoElement.textContent = currentText.substring(0, charIndex--);
+        if (charIndex < 0) {
+            isDeleting = false;
+            index = (index + 1) % textos.length;
+            setTimeout(typeEffect, typingSpeed);
+        } else {
+            setTimeout(typeEffect, typingSpeed / 2);
+        }
+    } else {
+        mudaTextoElement.textContent = currentText.substring(0, charIndex++);
+        if (charIndex > currentText.length) {
+            isDeleting = true;
+            setTimeout(typeEffect, pauseBetweenTexts);
+        } else {
+            setTimeout(typeEffect, typingSpeed);
+        }
+    }
+}
+
+typeEffect();
+
+document.addEventListener("DOMContentLoaded", () => {
+    const logos = document.querySelectorAll('.logo');
+    logos.forEach((logo, index) => {
+        logo.style.animationDelay = `${index * 0.2}s`;
+    });
+});
+
+
---
+++ bandup-tcc-kaua/nossa historia/historia.js
@@ -0,0 +1,14 @@
+// const paragrafos = document.querySelectorAll(".efeito-fadeIn");
+
+// document.addEventListener("scroll", function() {
+//     paragrafos.forEach(paragrafo => {
+//         if (visivel(paragrafo)) {
+//             paragrafo.classList.add("efeito-fadeIn--visible")
+//         }
+//     })
+// })
+
+// function visivel(elemento){
+//     const rect = elemento.getBoundingClientRect();
+//     return (rect.bottom > 0 && rect.top < (window.innerHeight - 150 || document.documentElement.clientHeight - 150));
+// }
